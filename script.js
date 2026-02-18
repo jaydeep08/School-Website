ce63560f143c25f60a0a565b14a1621d17290766
Jaydeep Patidar <89355189+jaydeep08@users.noreply.github.com>
2026-02-18 09:29:18 +0530

    2

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
+// SCROLL
 function scrollToSection(id) {
   document.getElementById(id)
     .scrollIntoView({ behavior: "smooth" });
 }
 
+// FORM
 function submitForm(e) {
   e.preventDefault();
-  alert("Thank you! We will contact you soon.");
+  alert("Message sent successfully!");
 }
+
+// DARK MODE
+const toggle = document.getElementById("themeToggle");
+
+toggle.onclick = () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    toggle.textContent = "☀️";
+  } else {
+    toggle.textContent = "🌙";
+  }
+};
